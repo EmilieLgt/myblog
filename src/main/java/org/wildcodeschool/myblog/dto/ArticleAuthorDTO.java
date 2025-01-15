e51bac0d9ded8d1b0269666cefb2dc55a5c2a2aa
EMILIE LINGAT <emilie.lingat@gmail.com>
2025-01-15 16:12:32 +0100

    quest8

---
+++ src/main/java/org/wildcodeschool/myblog/dto/ArticleAuthorDTO.java
@@ -0,0 +1,40 @@
+package org.wildcodeschool.myblog.dto;
+
+public class ArticleAuthorDTO {
+    private Long id;
+    private Long articleId;
+    private Long authorId;
+    private String contribution;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Long articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getContribution() {
+        return contribution;
+    }
+
+    public void setContribution(String contribution) {
+        this.contribution = contribution;
+    }
+}
